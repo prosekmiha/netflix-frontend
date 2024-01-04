659ba4827f5f95581532c5a7a4820737b01cc9fe
unknown <prosekmiha@gmail.com>
2024-01-04 19:20:55 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
